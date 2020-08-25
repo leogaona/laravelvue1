00b827a1991af82b2ce549e456b6642666f5f8e9
Leogaona <leogaona@gmail.com>
2020-08-24 23:53:47 -0400

    controlador y rutas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+Route::resource('tasks', 'TaskController', ['except' => 'show']);
+
